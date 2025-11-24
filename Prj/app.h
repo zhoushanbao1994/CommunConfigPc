49c8a59f1be9f924b746c76c5e0b9a60f9fad52d
zhoushanbao1994 <zhoushanbao@126.com>
2025-11-24 18:25:37 +0800

    调整点表窗体中一些常量的定义位置到App中

--- Prj/app.h
+++ Prj/app.h
@@ -40,6 +40,101 @@ public:
     } DevType_E;
 
 public:
+    // Modbus功能码
+    static const QString kModbusFunCode_01H;
+    static const QString kModbusFunCode_02H;
+    static const QString kModbusFunCode_03H;
+    static const QString kModbusFunCode_04H;
+
+    // Modbus数据类型
+    static const QString kModbusDateType_U16;
+    static const QString kModbusDateType_S16;
+    static const QString kModbusDateType_U32;
+    static const QString kModbusDateType_S32;
+    // DLT645 07数据标识
+    static const QString kDltDataId07_00000000;
+    static const QString kDltDataId07_00000100;
+    static const QString kDltDataId07_00000200;
+    static const QString kDltDataId07_00000300;
+    static const QString kDltDataId07_00000400;
+    static const QString kDltDataId07_00000500;
+    static const QString kDltDataId07_00000600;
+    static const QString kDltDataId07_00000700;
+    static const QString kDltDataId07_00000800;
+    //static const QString kDltDataId07_0000FF00;
+    static const QString kDltDataId07_00010000;
+    static const QString kDltDataId07_00010100;
+    static const QString kDltDataId07_00010200;
+    static const QString kDltDataId07_00010300;
+    static const QString kDltDataId07_00010400;
+    static const QString kDltDataId07_00010500;
+    static const QString kDltDataId07_00010600;
+    static const QString kDltDataId07_00010700;
+    static const QString kDltDataId07_00010800;
+    //static const QString kDltDataId07_0001FF00;
+    static const QString kDltDataId07_00020000;
+    static const QString kDltDataId07_00020100;
+    static const QString kDltDataId07_00020200;
+    static const QString kDltDataId07_00020300;
+    static const QString kDltDataId07_00020400;
+    static const QString kDltDataId07_00020500;
+    static const QString kDltDataId07_00020600;
+    static const QString kDltDataId07_00020700;
+    static const QString kDltDataId07_00020800;
+    //static const QString kDltDataId07_0002FF00;
+    static const QString kDltDataId07_01010000;
+    static const QString kDltDataId07_01010100;
+    static const QString kDltDataId07_01010200;
+    static const QString kDltDataId07_01010300;
+    static const QString kDltDataId07_01010400;
+    static const QString kDltDataId07_01010500;
+    static const QString kDltDataId07_01010600;
+    static const QString kDltDataId07_01010700;
+    static const QString kDltDataId07_01010800;
+    //static const QString kDltDataId07_0101FF00;
+    static const QString kDltDataId07_01020000;
+    static const QString kDltDataId07_01020100;
+    static const QString kDltDataId07_01020200;
+    static const QString kDltDataId07_01020300;
+    static const QString kDltDataId07_01020400;
+    static const QString kDltDataId07_01020500;
+    static const QString kDltDataId07_01020600;
+    static const QString kDltDataId07_01020700;
+    static const QString kDltDataId07_01020800;
+    //static const QString kDltDataId07_0102FF00;
+    static const QString kDltDataId07_02010100;
+    static const QString kDltDataId07_02010200;
+    static const QString kDltDataId07_02010300;
+    //static const QString kDltDataId07_0201FF00;
+    static const QString kDltDataId07_02020100;
+    static const QString kDltDataId07_02020200;
+    static const QString kDltDataId07_02020300;
+    //static const QString kDltDataId07_0202FF00;
+    static const QString kDltDataId07_02030000;
+    static const QString kDltDataId07_02030100;
+    static const QString kDltDataId07_02030200;
+    static const QString kDltDataId07_02030300;
+    //static const QString kDltDataId07_0203FF00;
+    static const QString kDltDataId07_02040000;
+    static const QString kDltDataId07_02040100;
+    static const QString kDltDataId07_02040200;
+    static const QString kDltDataId07_02040300;
+    //static const QString kDltDataId07_0204FF00;
+    static const QString kDltDataId07_02050000;
+    static const QString kDltDataId07_02050100;
+    static const QString kDltDataId07_02050200;
+    static const QString kDltDataId07_02050300;
+    //static const QString kDltDataId07_0205FF00;
+    static const QString kDltDataId07_02060000;
+    static const QString kDltDataId07_02060100;
+    static const QString kDltDataId07_02060200;
+    static const QString kDltDataId07_02060300;
+    //static const QString kDltDataId07_0206FF00;
+    static const QString kDltDataId07_02800002;
+    static const QString kDltDataId07_02800004;
+    static const QString kDltDataId07_02800005;
+
+public:
     explicit App(QObject *parent = nullptr);
 
     // 点表类型互转
